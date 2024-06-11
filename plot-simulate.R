5c3c6a179de914e1e721cb07308827921cbc7676
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2024-06-11 22:03:41 +0200

    Fix simulation I/O bug; restyle sim+expt vis

--- plot-simulate.R
+++ plot-simulate.R
@@ -3,14 +3,15 @@ library(gridExtra)
 library(igraph)
 library(ggraph)
 library(ggbeeswarm)
+library(dplyr)
+library(tidyr)
 
 ######## first a collection of example parameterisations
 
 # simulation code outputs trajectories to test[X].csv and adj mats to test[X].csv-am.csv, 
 # where X labels a particular example parameterisation
 
-# as of June 2024 there seems to be some issue with no data in the adj mat files. perhaps we haven't (re)run the trajectory analysis code?
-if(FALSE) {
+# read these in and visualise
 exptset = ""
 picset = 10
 g1 = g1a = g3 = list()
@@ -22,7 +23,7 @@ for(expt in 1:12) {
   sub = traj.df[traj.df$frame > max(traj.df$frame)-picset,]
   g1[[expt]] = ggplot(sub, aes(x=y,y=x,color=factor(mito)))  + 
     geom_path() +theme_void() +theme(legend.position="none") 
-
+  
   # subset just final point and plot these in green with trajectories
   sub1 = traj.df[traj.df$frame == max(traj.df$frame),]
   g1a[[expt]] = ggplot()  + geom_rect(aes(xmin = -2, xmax = 32, ymin = -2, ymax = 102), fill="black") +
@@ -61,6 +62,11 @@ gc = grid.arrange(grobs = g3, nrow=2)
 
 gac = grid.arrange(ga, gc, nrow=2)
 
+grid.arrange(g1a[[4]], g3[[4]],
+             g1a[[10]], g3[[10]],
+             g1a[[8]], g3[[8]],
+             g1a[[5]], g3[[5]])
+
 # simulation code also outputs summary statistics (mean min distance, mean edges) to outstats.csv
 stats.df = read.csv(paste(c("outstats", exptset, ".csv"), collapse=""))
 
@@ -77,7 +83,6 @@ myres = 2
 png(paste(c("examples", exptset, ".png"), collapse=""), width=1000*myres, height=400*myres, res=72*myres)
 grid.arrange(ga, gb, nrow=1, widths=c(2,1))  
 dev.off()
-}
 
 ######## now onto parameter scan
 
@@ -154,12 +159,12 @@ ghull = ggplot(samples, aes(x=x, y=y)) + geom_point() + geom_point(data=mins, ae
 
 # plot distributions of parameters that lie on the Pareto front
 gposts = grid.arrange(ggplot(mins, aes(x=D)) + geom_histogram(),
-             ggplot(mins, aes(x=kon)) + geom_histogram(),
-             ggplot(mins, aes(x=koff)) + geom_histogram(),
-             ggplot(mins, aes(x=V)) + geom_histogram(),
-             ggplot(mins, aes(x=dmito)) + geom_histogram(),
-             ggplot(mins, aes(x=kmito)) + geom_histogram(),
-             nrow=2)
+                      ggplot(mins, aes(x=kon)) + geom_histogram(),
+                      ggplot(mins, aes(x=koff)) + geom_histogram(),
+                      ggplot(mins, aes(x=V)) + geom_histogram(),
+                      ggplot(mins, aes(x=dmito)) + geom_histogram(),
+                      ggplot(mins, aes(x=kmito)) + geom_histogram(),
+                      nrow=2)
 
 png("inference-expt.png", width=800*myres, height=300*myres, res=72*myres)
 grid.arrange(ghull, gposts, nrow=1)
@@ -172,7 +177,7 @@ n.msh1 = 28
 n.friendly = 19
 
 # read in WT samples
-expts.df = expts.traj.df = data.frame()
+expts.df = expts.traj.df = ams.df = data.frame()
 for(i in 1:n.wt) {
   fname = paste(c("plant-mito-dynamics-main/mtgfp-rawtrajectories/mtGFP-", i, ".xml-stats.csv"), collapse="")
   tmp = read.csv(fname)
@@ -185,6 +190,11 @@ for(i in 1:n.wt) {
   tmp$elabel = i
   tmp$glabel = "WT"
   expts.traj.df = rbind(expts.traj.df, tmp)
+  fname = paste(c("plant-mito-dynamics-main/mtgfp-rawtrajectories/mtGFP-", i, ".xml-amlist.csv"), collapse="")
+  tmp = read.csv(fname)
+  tmp$elabel = i
+  tmp$glabel = "WT"
+  ams.df = rbind(ams.df, tmp)
 }
 
 # read in msh1 samples
@@ -200,6 +210,11 @@ for(i in 1:n.msh1) {
   tmp$elabel = i
   tmp$glabel = "msh1"
   expts.traj.df = rbind(expts.traj.df, tmp)
+  fname = paste(c("plant-mito-dynamics-main/msh1-rawtrajectories/MSH-", i, ".xml-amlist.csv"), collapse="")
+  tmp = read.csv(fname)
+  tmp$elabel = i
+  tmp$glabel = "msh1"
+  ams.df = rbind(ams.df, tmp)
 }
 
 # read in friendly samples
@@ -215,22 +230,66 @@ for(i in 1:n.friendly) {
   tmp$elabel = i
   tmp$glabel = "friendly"
   expts.traj.df = rbind(expts.traj.df, tmp)
+  fname = paste(c("plant-mito-dynamics-main/friendly-rawtrajectories/Friendly-", i, ".xml-amlist.csv"), collapse="")
+  tmp = read.csv(fname)
+  tmp$elabel = i
+  tmp$glabel = "friendly"
+  ams.df = rbind(ams.df, tmp)
 }
 
+# visualise (previously read) simulations and experiments together
+# a nice example set of experiments
+nice.g = c("WT", "WT", "msh1", "friendly")
+nice.e = c(3, 5, 1, 1)
+
+# construct cell and adjacency matrix visualisations for each
+cell.vis = am.vis = list()
+for(i in 1:length(nice.g)) {
+  this.g = nice.g[i]
+  this.e = nice.e[i]
+  
+  sub = expts.traj.df[expts.traj.df$glabel==this.g & expts.traj.df$elabel == this.e & expts.traj.df$t<=10,]
+  sub10 = sub[sub$t==10,]
+  
+  cell.vis[[i]] = ggplot()  + #geom_rect(aes(xmin = -2, xmax = 32, ymin = -2, ymax = 102), fill="black") +
+    geom_path(data=sub, aes(x=y,y=x,color=factor(traj)), alpha=0.5) + 
+    geom_point(data=sub10, aes(x=y,y=x), size=0.5, color="#66FF66") +theme_void() +theme(legend.position="none")  + 
+    #theme(plot.background = element_rect(fill = "black")) + 
+    theme(plot.margin = unit(c(.1,.1,.1,.1), "cm"),
+          panel.background = element_rect(fill = "black", color = "black")) + facet_wrap(~ elabel, scale="free")
+  
+  amg = graph_from_edgelist(as.matrix(ams.df[ams.df$glabel=="WT" & ams.df$elabel==i,2:3], directed=F))
+  
+  am.vis[[i]] = ggraph(amg, layout="nicely") + geom_edge_link(alpha=0.4, color="#AAAAAA") + geom_node_point(size=0.1) + theme_void() +
+    theme(plot.margin = unit(c(.1,.1,.1,.1), "cm"))
+}
+
+# pull a collection together for visualisation
+g.all.1 = ggarrange(g1a[[4]], g1a[[10]], g1a[[8]], g1a[[5]], nrow=1)
+g.all.2 = ggarrange(plotlist=cell.vis, nrow=1)
+g.all.3 = ggarrange(g3[[4]], g3[[10]], g3[[8]], g3[[5]], nrow=1)
+g.all.4 = ggarrange(plotlist = am.vis, nrow=1)
+
+sf = 2
+png("all-demo-image.png", width=800*sf, height=400*sf, res=72*sf)
+ggarrange(g.all.1, g.all.2, g.all.3, g.all.4, nrow=2, ncol=2, heights=c(2,1))
+dev.off()
+
+########## actual optimality analysis
+
+# get mito counts from different experiments
 nset.df = data.frame()
 for(glab in c("WT", "msh1", "friendly")) {
   for(elab in 1:max(expts.traj.df$elabel[expts.traj.df$glabel==glab])) {
     sub = expts.traj.df[expts.traj.df$glabel==glab & expts.traj.df$elabel == elab,]
-  nset.df = rbind(nset.df, data.frame(glabel=glab, elabel=elab, n=nrow(sub[sub$t==1,])))
+    nset.df = rbind(nset.df, data.frame(glabel=glab, elabel=elab, n=nrow(sub[sub$t==1,])))
   }
 }
 
-# so revisiting this, when we compare experimental data to simulation WITH SAME DIMENSIONS as the experiment
-# we often fall on the front. but there's a subtlety about # of mitos vs # of trajectories to resolve
-
-# for comparing simulation and experiment
+# timeframe for comparing simulation and experiment
 key.frame = 100
 
+# loop through experiments and compare to subset of simulations with comparable geometry
 complist = list()
 for(glab in c("WT", "msh1", "friendly")) {
   for(elab in 1:max(expts.traj.df$elabel[expts.traj.df$glabel==glab])) {
@@ -242,19 +301,12 @@ for(glab in c("WT", "msh1", "friendly")) {
     x.dim = max(pca$scores[,1])-min(pca$scores[,1])
     y.dim = max(pca$scores[,2])-min(pca$scores[,2])
     n.dim = nset.df$n[nset.df$glabel==glab & nset.df$elabel==elab]
-  #  if(trajmult == 0) {
-  #    n.dim = 100
-  #  } else {
-  #    n.dim = max(df.sub$traj*trajmult)
-  #  }
     
-  #  subbing = samples[((samples$Cx > x.dim-10 & samples$Cx < x.dim+10) & 
-  #                       (samples$Cy > y.dim-10 & samples$Cy < y.dim+10) & 
-  #                       (samples$Cn > n.dim-10 & samples$Cn < n.dim+10)),]
+    # get subset of simulations with comparable geometry
     subbing = stats.df[((stats.df$Cx > x.dim-10 & stats.df$Cx < x.dim+10) & 
-                      (stats.df$Cy > y.dim-10 & stats.df$Cy < y.dim+10) & 
-                      (stats.df$Cn > n.dim-10 & stats.df$Cn < n.dim+10) &
-                        (stats.df$V <= 2 & stats.df$kmito*stats.df$D < 0.2)),]
+                          (stats.df$Cy > y.dim-10 & stats.df$Cy < y.dim+10) & 
+                          (stats.df$Cn > n.dim-10 & stats.df$Cn < n.dim+10) &
+                          (stats.df$V <= 2 & stats.df$kmito*stats.df$D < 0.2)),]
     
     titlestr = paste0(glab, "-", elab, ": ", round(x.dim), "x", round(y.dim), "x", n.dim, collapse=" ")
     complist[[length(complist)+1]] = ggplot() + 
@@ -266,23 +318,24 @@ for(glab in c("WT", "msh1", "friendly")) {
       theme_light() + theme(legend.position = "none") + 
       xlab("Physical clumping (log 1/meanmin)") + ylab("Exchange isolation (log 1/meanedges)") +
       ggtitle(titlestr)
-      
+    
     
   }
 }
 
+# construct megaplot of each of these
 png(paste0("ind-trajs-", trajmult, ".png", collapse=""), width=1000*sf, height=1200*sf, res=72*sf)
 ggarrange(plotlist = complist)
 dev.off()
 
-samples$rho = samples$Cn/(samples$Cx*samples$Cy)
+######## pooled morphospace
 
-# here we don't look very Pareto -- but of course this isn't conditioned on density (as of June 2024 we have reparameterised the sims though, so it does look Pareto)
 # decide on a Pareto summary stat
-samples$stat = samples$meanedges / samples$meanmin #/ samples$meanedges
+samples$stat = samples$meanedges / samples$meanmin 
 ggplot() +
   geom_point(data=samples, aes(x=x,y=y,color=log(stat))) 
 
+# plot the pooled space with experimental data
 expts.df$glabel = factor(expts.df$glabel, levels=c("WT", "msh1", "friendly"))
 g.all.1 = ggplot() +
   geom_point(data=samples[samples$V<=2 & samples$kmito*samples$D < 0.1 & samples$Cn < 150,], aes(x=x,y=y,color=log(stat)), alpha=1) +
@@ -292,15 +345,16 @@ g.all.1 = ggplot() +
   labs(x="Clumping [log(1/min dist)]", y="Loneliness [log(1/mean degree)]", color="Pareto statistic", fill="Experiment") +
   theme_minimal()
 
+# plot zoomed-in version
 g.all.2 = ggplot() +
   geom_point(data=samples[samples$V<=2 & samples$kmito*samples$D < 0.1 & samples$Cn < 150,], aes(x=x,y=y,color=log(stat)), alpha=0.25, size=10) +
   scale_color_viridis() +
   geom_point(data=expts.df[expts.df$frame==key.frame,], 
-             aes(x = log(1/mean.min.dist), y=log(1/mean.degree), fill=glabel), size=3, shape=21) + 
+             aes(x = log(1/mean.min.dist), y=log(1/mean.degree), fill=glabel), size=2, shape=21) + 
   labs(x="Clumping [log(1/min dist)]", y="Loneliness [log(1/mean degree)]", color="Pareto statistic", fill="Experiment") +
   theme_minimal() + xlim(-1.5,0) + ylim(-2.5, -0.75) + theme(legend.position = "none")
 
-
+# plot showing differences in Pareto stat (plus followup stats)
 sub.expts = expts.df[expts.df$frame==key.frame,]
 sub.expts$stat = (sub.expts$mean.degree/sub.expts$mean.min.dist) #log(sub.expts$mean.min.dist / sub.expts$mean.degree)
 g.all.3 = ggplot(sub.expts, aes(x=glabel, y=stat, fill=glabel, color=glabel)) + 
@@ -315,11 +369,42 @@ wilcox.test(sub.expts$stat[sub.expts$glabel=="WT"], sub.expts$stat[sub.expts$gla
 wilcox.test(sub.expts$stat[sub.expts$glabel=="WT"], sub.expts$stat[sub.expts$glabel=="friendly"])
 wilcox.test(sub.expts$stat[sub.expts$glabel=="friendly"], sub.expts$stat[sub.expts$glabel=="msh1"])
 
+# megaplot of the morphospace picture
 ggarrange( g.all.1, ggarrange(g.all.2, g.all.3, nrow=2, labels=c("B", "C")), labels=c("A", ""), nrow=1, widths=c(1.5,1))
 
+############# set-valued optimisation
+#### still a WIP
+summary_df <- samples %>%
+  group_by(group) %>%
+  summarize(
+    mean_value = mean(value, na.rm = TRUE),
+    sd_value = sd(value, na.rm = TRUE)
+  )
 
-# June 2024 -- interesting part -- what determines Pareto structure
-#### exploring determinants of Pareto front
+########### "inference without data"
+# further inference, based on Pareto front and experimental observations
+
+# get the mean wildtype behaviour
+mean.x = mean(expts.df$mean.min.dist[expts.df$glabel=="WT"])
+mean.y = mean(expts.df$mean.degree[expts.df$glabel=="WT"])
+
+# subset simulations close to this
+# previously we built "mins", a dataframe of simulations on the Pareto front
+delta = 2
+inv.set = mins[abs(mins$meanmin-mean.x) < delta & abs(mins$meanedges-mean.y) < delta,]
+samples$hist.ref = "All"
+mins$hist.ref = "Pareto"
+inv.set$hist.ref = "Proximal"
+hist.set = rbind(samples, mins, inv.set)
+new.hist.set = hist.set %>% pivot_longer(cols=c("D", "kon", "koff", "V", "dmito", "kmito", "inter"))
+
+# plot parameters in each class of closeness
+ggplot(new.hist.set, aes(x=value, y=after_stat(density), fill=hist.ref)) +
+  geom_histogram(position = "dodge", width=2) + 
+  facet_wrap(~name, scales = "free") +
+  theme_minimal()
+
+####### exploring parameter determinants of Pareto front
 baseplot = ggplot(data=samples, aes(x=x,y=y)) + scale_color_gradient(low = "blue", high = "red") + theme_light() #+ theme(legend.position = "none") 
 
 p.alpha = 0.2
