01556a38a0bdff8b910b56f2377cf59b0d1350dc
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2026-09-07 20:59:10 +0100

    Pipeline wrapper

---
+++ pipeline.sh
@@ -0,0 +1,31 @@
+# pull existing experimental data and analysis script 
+git clone https://github.com/StochasticBiology/plant-mito-dynamics
+
+# run analysis script on existing experimental data
+cd plant-mito-dynamics
+find . -name "*[0-9].xml" -print0 | while read -d $'\0' file
+do  
+    Rscript trajectory-analysis.R "$file" 1.6 10 50 100
+done
+
+# put analysis script in folder with new data
+cp trajectory-analysis.R ../cipro-rawtrajectories
+
+# run analysis script on new data
+cd ../cipro-rawtrajectories
+find . -name "*[0-9].xml" -print0 | while read -d $'\0' file
+do  
+    Rscript trajectory-analysis.R "$file" 1.6 10 50 100
+done
+
+# run simulations
+gcc -o3 simulate.c -lm -o simulate.ce
+
+./simulate.ce 1 > tmp1 &
+./simulate.ce 2 > tmp2 &
+./simulate.ce 3 > tmp3 &
+./simulate.ce 4 > tmp4 &
+./simulate.ce 5 > tmp5 &
+
+# run analysis and visualisation code
+Rscript plot-simulate-cipro.R
